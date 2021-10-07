8181162dd59626dca4e0a0840567e29318798f3f
Jhonatan Andrés Beltrán <jhonariston@gmail.com>
2021-10-06 20:55:46 -0500

    Add files via upload
    pasando productos con base de datos h2 sql

---
+++ Springboot-servicio-productos/target/classes/import.sql
@@ -0,0 +1,3 @@
+ INSERT INTO  productos (nombre, precio, create_at) VALUES('tres leches', 200, NOW());
+ INSERT INTO  productos (nombre, precio, create_at) VALUES('torta de helado oreo', 400, NOW());
+ INSERT INTO  productos (nombre, precio, create_at) VALUES('pie de limon', 350, NOW());
